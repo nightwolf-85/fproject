841064c022a2723cb3c689ab0c4492097f52dea1
nightwolf-85 <75392411+nightwolf-85@users.noreply.github.com>
2021-10-19 18:43:21 +0300

    Update variables

---
+++ variables.tf
@@ -0,0 +1,54 @@
+variable "DB_USER" {
+  default = "mongodb_user"
+  description = "mongodb username"
+}
+
+variable "DB_PASSWORD" {
+  default = "Aa123456@"
+  description = "mongodb password"
+}
+
+variable "DB_URL" {
+  default = ""
+  description = "mongodb URL"
+}
+
+variable "DB_URL_NOMAD" {
+  default = ""
+  description = "mongodb URL for the nomad cluster for the webblog demo"
+}
+
+variable "DB_URL_AZURE" {
+  default = ""
+  description = "mongodb URL for the webblog demo on Azure for end-to-end deployment"
+}
+
+variable "subscription_id" {
+  default = "3012021f-b102-437a-81f9-fb3cfb760017"
+  description = "Azure subscription_id"
+}
+
+variable "tenant_id" {
+  default = "55189ca1-1c81-45f3-9ed7-9c6e6f8cc22b"
+  description = "Azure tenant_id"
+}
+
+variable "client_secret" {
+  default = "1CO7Q~K9tnxEoLZ1i6XNVZ7L9Heui8Tci4ocK"
+  description = "Azure client_secret"
+}
+
+variable "client_id" {
+  default = "23c9c05e-96ae-4dd4-9c3b-a47dceb5ff14"
+  description = "Azure client_id"
+}
+
+variable "prefix" {
+  description = "This prefix will be included in the name of most resources."
+  default     = "project"
+}
+
+variable "location" {
+  description = "The region where the virtual network is created."
+  default     = "centralus"
+}
